bb8246836d18d46b057fb07cfcda21b7c41c224c
Dan Roundhill <danroundhill@gmail.com>
2012-05-15 12:24:41 +0100

    Update to WordPress Plugin
    Returns 'No more events' if there are no remaining events posts.

--- WordPress plugin/rehope.php
+++ WordPress plugin/rehope.php
@@ -33,9 +33,22 @@ function get_home_tab_data( $args ) {
 }
 
 function get_events( $args ) {
+	global $wpdb;
 	//get the 10 latest posts from the 'Events' category
-	 $args = array( 'numberposts' => 20, 'category' => 5, 'meta_key' => 'date', 'orderby' => 'meta_value', 'order'=> 'ASC');
-	 $lastposts = get_posts($args);
+	$querystr = "
+		SELECT wposts.*
+		FROM $wpdb->posts wposts
+		LEFT JOIN $wpdb->postmeta wpostmeta ON wposts.ID = wpostmeta.post_id 
+		LEFT JOIN $wpdb->term_relationships ON (wposts.ID = $wpdb->term_relationships.object_id)
+		LEFT JOIN $wpdb->term_taxonomy ON ($wpdb->term_relationships.term_taxonomy_id = $wpdb->term_taxonomy.term_taxonomy_id)
+		WHERE $wpdb->term_taxonomy.taxonomy = 'category'
+		AND $wpdb->term_taxonomy.term_id = 5
+		AND wpostmeta.meta_key = 'date'
+		AND wposts.post_type = 'post'
+		AND wposts.post_status = 'publish'
+		ORDER BY wpostmeta.meta_value ASC
+	";
+	$lastposts = $wpdb->get_results($querystr, OBJECT);
 	 $ctr = 0;
 	 foreach( $lastposts as $post ) {
 		$date_str = get_post_meta($post->ID, 'date', true);
@@ -48,32 +61,28 @@ function get_events( $args ) {
 			$ctr++;	
 		}
 	 }
-
-    return $events;
-}
-
-function get_events_test( $args ) {
-	//get the 10 latest posts from the 'Events' category
-	 $args = array( 'numberposts' => 20, 'category' => 5, 'meta_key' => 'date', 'orderby' => 'meta_value', 'order'=> 'ASC');
-	 $lastposts = get_posts($args);
-	 $ctr = 0;
-	 foreach( $lastposts as $post ) {
-		$date_str = get_post_meta($post->ID, 'date', true);
-		if ( (strtotime( $date_str ) + 86400) > time() ) {
-			$events[$ctr]['image'] = get_post_image_url( $post->post_content );
-			$events[$ctr]['title']  = $post->post_title;
-		    $events[$ctr]['content'] = $post->post_excerpt;
-			$events[$ctr]['date']  = $date_str;	
-			$events[$ctr]['city_link']  = get_post_meta($post->ID, 'city_link', true);
-			$ctr++;	
+	
+	if ( !isset($events) ) {
+		for ( $i = 0; $i < 2; $i++ ) {
+			$events[$i]['image'] = 'path-to-placeholder-jpg-here';
+			$events[$i]['title']  = 'No Upcoming Events';
+		    $events[$i]['content'] = 'There are no more upcoming events. Check back soon!';
+			$events[$i]['date']  = '2012-07-01';	
+			$events[$i]['city_link']  = 'http://rehope.co.uk';
 		}
-	 }
+	} else if ( count($events) == 1 ) {
+		$events[1]['image'] = 'path-to-placeholder-jpg-here';
+		$events[1]['title']  = 'No More Upcoming Events';
+	    $events[1]['content'] = 'There are no more upcoming events. Check back soon!';
+		$events[1]['date']  = '2012-07-01';	
+		$events[1]['city_link']  = 'http://rehope.co.uk';
+	}
 
     return $events;
 }
 
 function get_messages( $args ) {
-	//get the 10 latest posts from the 'Events' category
+	//get the 10 latest posts from the 'Messages' category
 	 $lastposts = get_posts('numberposts=10&category=6');
 	 $ctr = 0;
 	 foreach( $lastposts as $post ) {
